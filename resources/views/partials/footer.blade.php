e73cafd210c833e2cac6331ab1fbffeed39a16a9
LarisaGimbu <gimbularisa@gmail.com>
2022-01-21 17:06:27 +0100

    footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,56 @@
 <footer>
-  <h1>Footer</h1>
+  <div class="top-footer">
+    <div class="container">
+      <div class="left-content">
+        <ul>
+          <li><h3>dc comics</h3></li>
+          <li>Characters</li>
+          <li>Comics</li>
+          <li>Movies</li>
+          <li>TV</li>
+          <li>Games</li>
+          <li>Videos</li>
+          <li>News</li>
+        </ul>
+        <ul>
+          <li><h3>dc</h3></li>
+          <li>Terms Of Use</li>
+          <li>Pravacy policy (New)</li>
+          <li>Ad Choice</li>
+          <li>Advertising</li>
+          <li>Jobs</li>
+          <li>Subscriptions</li>
+          <li>Talent Workshops</li>
+          <li>CPSC Certificates</li>
+          <li>Ratings</li>
+          <li>Shop Help</li>
+          <li>Contact Us</li>
+        </ul>
+        <ul>
+          <li><h3>Sites</h3></li>
+          <li>DC</li>
+          <li>MAD Magazine</li>
+          <li>SC Kids</li>
+          <li>DC Universe</li>
+          <li>DC Power Visa</li>
+        </ul>
+      </div>
+      <div class="right-content">
+        <img src="{{asset('img/dc-logo-bg.png') }}" alt="">
+      </div>
+    </div>
+  </div>
+  <div class="bottom-footer">
+    <div class="container">
+      <a class="btn" href="">sign-up now!</a>
+      <div class="follow-box">
+        <span>FOLLOW US</span>
+        <i class="fab fa-facebook-f"></i>
+        <i class="fab fa-twitter"></i>
+        <i class="fab fa-youtube"></i>
+        <i class="fab fa-pinterest-p"></i>
+        <i class="fas fa-map-marker-alt"></i>
+      </div>
+    </div>
+  </div>
 </footer>
